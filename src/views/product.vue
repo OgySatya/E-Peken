7ee4d28405129e05a5c8a87b17e4cada00c30147
OgySatya <162892731+OgySatya@users.noreply.github.com>
2024-04-21 22:32:17 +0700

    Update product.vue

--- src/views/product.vue
+++ src/views/product.vue
@@ -1,8 +1,15 @@
 <script setup>
 import { ref, computed, onMounted } from 'vue';
 
-const allProduct = ref()
-const cariSesuatu = ref()
+const allProduct = ref([])
+const cari = ref('')
+const categories = ref([
+    "electronics",
+    "jewelery",
+    "men's clothing",
+    "women's clothing"
+])
+const selected = ref('')
 onMounted(() => {
     getProduct()
 })
@@ -11,16 +18,21 @@ async function getProduct() {
     const data = await response.json();
     allProduct.value = data
 }
+async function getProductCategory(param) {
+    const response = await fetch(`https://fakestoreapi.com/products/category/${param}`);
+    const data = await response.json();
+    allProduct.value = data
+}
 const currentPage = ref(1);
 
 const totalPages = computed(() => {
-    return Math.ceil(allProduct.value.length / 6);
+    return Math.ceil(filteredName.value.length / 6);
 });
 
 const paginatedData = computed(() => {
     const startIndex = (currentPage.value - 1) * 6;
     const endIndex = startIndex + 6;
-    return allProduct.value.slice(startIndex, endIndex);
+    return filteredName.value.slice(startIndex, endIndex);
 });
 
 function previousPage() {
@@ -34,15 +46,17 @@ function nextPage() {
         currentPage.value++;
     }
 }
+
 const filteredName = computed(() => {
-    return allProduct.value.filter(n => n.title ? n.title.toLowerCase().includes(cariSesuatu.value.toLowerCase()) : ""
+    return allProduct.value.filter(name => name.title ?
+        name.title.toLowerCase().includes(cari.value.toLowerCase()) : ''
     )
 })
 
 </script>
 <template>
     <form class="flex w-1/3 items-center mx-auto mt-5" @submit.prevent="newSearch">
-        <input v-model="cariSesuatu" type="text" placeholder="Cari Sesuatu"
+        <input v-model="cari" type="text" placeholder="Cari Sesuatu"
             class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" />
         <button type="submit"
             class="p-2.5 ms-2 font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
@@ -52,21 +66,30 @@ const filteredName = computed(() => {
             </svg>
         </button>
     </form>
-    <div class="flex items-center justify-center py-4 md:py-8 flex-wrap">
-        <button type="button"
-            class="text-blue-700 hover:text-white border border-blue-600 bg-white hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-full text-base font-medium px-5 py-2.5 text-center me-3 mb-3 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:bg-gray-900 dark:focus:ring-blue-800">All
-            categories</button>
-        <button type="button"
-            class="text-gray-900 border border-white hover:border-gray-200 dark:border-gray-900 dark:bg-gray-900 dark:hover:border-gray-700 bg-white focus:ring-4 focus:outline-none focus:ring-gray-300 rounded-full text-base font-medium px-5 py-2.5 text-center me-3 mb-3 dark:text-white dark:focus:ring-gray-800">Shoes</button>
-        <button type="button"
-            class="text-gray-900 border border-white hover:border-gray-200 dark:border-gray-900 dark:bg-gray-900 dark:hover:border-gray-700 bg-white focus:ring-4 focus:outline-none focus:ring-gray-300 rounded-full text-base font-medium px-5 py-2.5 text-center me-3 mb-3 dark:text-white dark:focus:ring-gray-800">Bags</button>
-        <button type="button"
-            class="text-gray-900 border border-white hover:border-gray-200 dark:border-gray-900 dark:bg-gray-900 dark:hover:border-gray-700 bg-white focus:ring-4 focus:outline-none focus:ring-gray-300 rounded-full text-base font-medium px-5 py-2.5 text-center me-3 mb-3 dark:text-white dark:focus:ring-gray-800">Electronics</button>
-        <button type="button"
-            class="text-gray-900 border border-white hover:border-gray-200 dark:border-gray-900 dark:bg-gray-900 dark:hover:border-gray-700 bg-white focus:ring-4 focus:outline-none focus:ring-gray-300 rounded-full text-base font-medium px-5 py-2.5 text-center me-3 mb-3 dark:text-white dark:focus:ring-gray-800">Gaming</button>
-    </div>
+
+    <ul
+        class="items-center w-3/4 mx-auto my-3 font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:flex">
+        <li class="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
+            <div class="flex items-center ps-3">
+                <input type="radio" v-model="selected" value="" @change="getProduct()"
+                    class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500">
+                <label class="w-full py-1 ms-2 mx-auto my-1 font-medium text-gray-900 dark:text-gray-300">All Categories
+                </label>
+            </div>
+        </li>
+        <li v-for="category in categories"
+            class="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
+            <div class="flex items-center ps-3">
+                <input type="radio" v-model="selected" :value=category @change="getProductCategory(selected)"
+                    class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500">
+                <label class="w-full capitalize py-1 ms-2 mx-auto my-1 font-medium text-gray-900 dark:text-gray-300">{{
+        category }}
+                </label>
+            </div>
+        </li>
+    </ul>
     <v-list class="grid grid-cols-3 gap-4 w-3/4 mx-auto">
-        <v-list-item v-for="product in filteredName" :key="product.id"
+        <v-list-item v-for="product in paginatedData" :key="product.id"
             class="border-gray-400 border-4 rounded-lg p-5 hover:border-yellow-700">
             <RouterLink :to="{ name: 'productinfo', params: { id: product.id } }">
                 <div class="h-64 overflow-hidden content-center">
